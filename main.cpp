674a8dd82b3d66dfdbac03ae160bd5bd0ee3fc39
Kenma7 <faishalmuhammad233@gmail.com>
2023-10-16 21:19:00 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<<"\n------------------------------------------------------------------------------------------------------------------------";
+
+//ini pake <<endl buat bikin spasi/baris baru
+ cout <<"Nama saya : Faishal Muhammad Farhan" <<endl <<endl;
+ cout <<"NPM : 2310631170135" <<endl<<endl;
+
+//ini pake \n buat newline/garis baru
+ cout <<"Kelas : C - Informatika\n\n";
+ cout <<"Cita - Cita : Buat startup big3\n";
+ cout<<"------------------------------------------------------------------------------------------------------------------------";
+ return 0;
+}
