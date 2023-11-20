b688c4748ba41b5f744e47fd9302f5dd1df6ecfa
Kenma7 <147836756+Kenma7@users.noreply.github.com>
2023-11-20 23:52:45 +0700

    Add files via upload

---
+++ Pertemuan_1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//buat looping "-"
+void loopFunction(){
+    for (int i = 0; i < 115; ++i){
+            cout << "-";
+    }}
+
+int main(){
+
+    loopFunction();
+
+    //ini pake <<endl buat bikin spasi/baris baru
+        cout <<"\nNama saya : Faishal Muhammad Farhan" <<endl <<endl;
+        cout <<"NPM : 2310631170135" <<endl<<endl;
+
+    //ini pake \n buat newline/garis baru
+        cout <<"Kelas : C - Informatika\n\n";
+        cout <<"Cita - Cita : Full Stack Dev\n\n";
+        cout <<"Motivasi masuk informatika : Pengen Kerja Remote\n";
+
+    loopFunction();
+ return 0;
+}
